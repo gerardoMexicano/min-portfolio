9e2e3a84139327945c64cc05e80095996bfa5d65
Gerardo <Germx06@gmail.com>
2024-04-10 17:17:07 -0600

    buil v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        build: {
+          assetsInlineLimit: 10240, // Cambiado a 10 KB
+          
+        },ssr: {
+            inline: true, // Habilita la incrustación de scripts en línea durante la generación del servidor
+          }
+        
+      } 
+});
